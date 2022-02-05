599351eac0a9deb719ec13093f8dadc7ac09a180
Chanh Le <chanh@flodesk.com>
2022-02-05 18:21:17 +0700

    post new comment

--- src/frontend/comment/index.js
+++ src/frontend/comment/index.js
@@ -1,6 +1,13 @@
-import template from './index.html';
+import html from './index.html';
 import './index.css';
 import db from './db';
+import auth from './auth';
+
+const anonymousAvatar = '/assets/images/elizabeth.jpg';
+const anonymousUsername = 'Khách ghé chơi';
+let currentUser = { displayName: anonymousUsername, photoURL: anonymousAvatar };
+
+const template = html.replaceAll('$avatar', anonymousAvatar);
 
 const postUrl = window.location.href;
 const postSlug = postUrl.split('/')[4];
@@ -38,7 +45,7 @@ db.getAll(postCommentUrl, 'time',
     fileredContent = filterURLinComment(fileredContent);
 
     const cmtItemElem = cmtItemTpl.cloneNode(true);
-    cmtItemElem.style.display = 'block';
+    cmtItemElem.style.removeProperty('display');
     cmtItemElem.querySelector('.avatar > img').src = avatar;
     cmtItemElem.querySelector('.metadata > b').innerText = username;
     cmtItemElem.querySelector('.metadata > span').innerText = commentTime;
@@ -47,3 +54,47 @@ db.getAll(postCommentUrl, 'time',
     cmtListElem.appendChild(cmtItemElem);
   },
 );
+
+
+const loginBox = document.getElementById('login-box');
+loginBox.querySelector('button').onclick = () => {
+  auth.loginWithGoogle();
+};
+auth.onAuthStateChanged('loginBox', (authState, user) => {
+  console.log(authState, user);
+  currentUser = user ? user : currentUser;
+  const display = authState === auth.AuthState.No ? 'block' : 'none';
+  loginBox.style.display = display;
+});
+
+const cmtInputElem = document.querySelector('#comment-box textarea');
+const cmtInputOnKeyUp = e => {
+  if (e.target.scrollHeight <= 50) {
+    return;
+  }
+
+  e.target.style.height = '5px';
+  e.target.style.height = (e.target.scrollHeight + 10) + 'px';
+};
+
+const cmtInputOnKeyDown = e => {
+  let keyCode = e.which || e.keyCode;
+  let ctrlCode = e.ctrlKey || e.metaKey;
+  const comment = cmtInputElem.value;
+
+  if (keyCode !== 13 || !ctrlCode) {
+    return;
+  }
+
+  let commentData = {
+    user: currentUser.displayName,
+    avatar: currentUser.photoURL,
+    time: (new Date()).getTime(),
+    content: encodeHTML(comment)
+  };
+  db.create(postCommentUrl, commentData);
+  cmtInputElem.value = '';
+};
+
+cmtInputElem.addEventListener('key-up', cmtInputOnKeyUp);
+cmtInputElem.addEventListener('key-down', cmtInputOnKeyDown);

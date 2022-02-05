3aaf5e2aa7f5742e980d6c115b564449e2d810ee
Chanh Le <chanh@flodesk.com>
2022-02-05 21:33:36 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import fs from 'fs';
 import { promisify } from 'util';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import moment from 'moment';
+
 import publishData from './publish.json';
 import md2Html from './md2html';
 
@@ -18,10 +20,27 @@ app.set('view engine', 'twig');
 // Twig.js has support for this using the {% extends parent %} tag
 app.set('view options', { layout: false });
 
+const formatPublishedDate = str => {
+  const date = moment(str).toDate();
+  const dateStr = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;  
+  const dateStrs = date.toDateString().split(' ');
+  const prettyDateStr = `${dateStrs[0]}, ${dateStrs[1]} ${dateStrs[2]}, ${dateStrs[3]}`;
+
+  return {
+    dateStr,
+    prettyDateStr,
+  };
+};
+
 app.get('/', (_req, res) => {
   const postLists = publishData.years.map(year => ({
     year,
-    posts: publishData.posts.filter(post => post.date.includes(year))
+    posts: publishData.posts
+      .filter(post => post.date.includes(year))
+      .map(p => {
+        const d = formatPublishedDate(p.date);
+        return { ...p, ...d };
+      })
   }));
 
   res.render('home', { postLists });
@@ -37,7 +56,7 @@ app.get('/p/:slug', async (req, res) => {
 
   const mdData = await fsReadFile(`${__dirname}/posts/${postSlug}/index.md`);
   const postContent = md2Html(mdData);
-  
+
   res.render('post', {
     title: post.title,
     content: postContent,
